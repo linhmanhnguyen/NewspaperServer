10241a7da7c9de6cfe8d5518a099fb1d027d4460
linhmanh <linhmanhng@gmail.com>
2023-11-25 01:38:44 +0700

    feat: quên mật khẩu, gửi mật khẩu mới về gmail

--- Project/account.js
+++ Project/account.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3300;
 const db = require('./connect');
+
+const nodemailer = require('nodemailer');
+const passport = require('passport');
+
 var bodyParser = require('body-parser');
 
 var md5 = require('md5');
@@ -74,8 +78,6 @@ app.post('/accounts/register', (req, res) => {
 app.put('/accounts/changePassword', (req, res) => {
 
     const { email, oldPassword, newPassword } = req.body;
-    console.log(req.body)
-    console.log(md5(oldPassword))
 
     db.query('SELECT * FROM Accounts WHERE email = ? AND password_of_user = ?', [email, md5(oldPassword)], (error, results) => {
         if (error) {
@@ -96,6 +98,56 @@ app.put('/accounts/changePassword', (req, res) => {
         }
     });
 });
+
+const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, 
+    auth: {
+        user: 'newspaperappdemo@gmail.com',
+        pass: 'erdg yukn fbof npxq',
+    }
+  });
+  
+  function randomPassword() {
+      const length = 10;
+      const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      let newPassword = '';
+      for (let i = 0; i < length; i++) {
+        newPassword += characters.charAt(Math.floor(Math.random() * characters.length));
+      }
+      return newPassword;
+    }
+  
+  app.post('/accounts/forgotPassword', (req, res) => {
+      const { email } = req.body;
+  
+      const newPassword = randomPassword();
+    
+      const mailOptions = {
+        from: "Newspaper",
+        to: email,
+        subject: 'Thông báo mật khẩu mới',
+        text: `Mật khẩu mới của bạn là: ${newPassword}`
+      };
+    
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.error('Lỗi khi gửi email:', error);
+          res.status(500).send('Lỗi server');
+        } else {
+          db.query('UPDATE Accounts SET password_of_user = ? WHERE email = ?', [md5(newPassword), email], (updateError, updateResults) => {
+            if (updateError) {
+              console.error('Lỗi khi cập nhật mật khẩu:', updateError);
+              res.status(500).send('Lỗi server khi cập nhật mật khẩu');
+            } else {
+              res.json({ message: 'Email đã được gửi thành công, vui lòng kiểm tra hộp thư đến của bạn.' });
+              console.log(newPassword);
+            }
+          });
+        }
+      });
+  });
 
 app.get('/accounts', (req, res) => {
     db.query('SELECT * FROM Accounts', (error, results) => {
--- Project/connect.js
+++ Project/connect.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection =  mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '011602',
+    password: 'aB011602',
     database: 'Newspaper'
 })
 
